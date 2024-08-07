b96c5abf15db323376ce8c5117f2cb15c9e69215
Jaivik Bhanushali <bhanushalijaiv@gmail.com>
2024-08-07 16:10:39 +0530

    Update project links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,17 @@ import { IProjectItem, ProjectType, RepoType } from "@/types";
 
 const projects: IProjectItem[] = [
   {
+    id: "finance-app",
+    title: "Finance App",
+    description: "I developed a finance app using React-Ionic and Express-Node. It tracks money given to or received from clients, with daily, monthly, and yearly interest calculations. The app provides summaries of total transactions, remaining capital, and interest owed, making financial management easy and transparent.",
+    icon: "/skills/ionic.svg",
+    repoType: RepoType.Private,
+    projectType: ProjectType.Freelance,
+    // githubUrl: "",
+    // url: "",
+    tags: ["Ionic", "ReactJS", "NodeJS", "Express.js", "MongoDB"],
+  },
+  {
     id: "e-commerce-website",
     title: "E-commerce Website",
     description: "I developed a robust e-commerce website using WordPress and WooCommerce, featuring all essential functionalities and integrated Paytm for secure payments. The site is optimized for performance and user experience, ensuring a seamless shopping experience.",
@@ -9,7 +20,7 @@ const projects: IProjectItem[] = [
     repoType: RepoType.Public,
     projectType: ProjectType.Freelance,
     // githubUrl: "",
-    url: "www.secretjunction.com",
+    url: "https://secretjunction.com",
     tags: ["Wordpress", "Woo-Commerce", "PHP", "Paytm"],
   },
   {
@@ -20,19 +31,8 @@ const projects: IProjectItem[] = [
     repoType: RepoType.Public,
     projectType: ProjectType.Freelance,
     // githubUrl: "",
-    url: "www.sanandam.com",
+    url: "https://sanandam.com/",
     tags: ["Wordpress", "PHP", "CSS"],
-  },
-  {
-    id: "finance-app",
-    title: "Finance App",
-    description: "I developed a finance app using React-Ionic and Express-Node. It tracks money given to or received from clients, with daily, monthly, and yearly interest calculations. The app provides summaries of total transactions, remaining capital, and interest owed, making financial management easy and transparent.",
-    icon: "/skills/ionic.svg",
-    repoType: RepoType.Private,
-    projectType: ProjectType.Freelance,
-    // githubUrl: "",
-    // url: "",
-    tags: ["Ionic", "ReactJS", "NodeJS", "Express.js", "MongoDB"],
   },
   {
     id: "stock-market-strategy-dashboard",
